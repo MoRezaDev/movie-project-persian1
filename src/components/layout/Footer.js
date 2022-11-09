f878c12038486daab31e1e583ab44d7e384a51fb
Mohammad Reza Akhshabi <MoReza.Dev@gmail.com>
2022-11-09 23:13:55 +0330

    footer small device bug fixed

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useContext } from "react";
+import { sizeContext } from "../../context/WindowSizeProvider";
 
 const styles = {
   backgroundColor: "#2d2c2c",
@@ -17,13 +19,19 @@ const styles2 = {
   padding: "1rem",
   cursor: "pointer",
   borderRadius: ".5rem",
-  lineHeight: "2px",
+  lineHeight: "15px",
 };
 
 const Footer = () => {
+  const { size } = useContext(sizeContext);
   return (
     <div style={styles}>
-      <div style={styles2}>&copy; 2022 Developed by: Moreza.dev@gmail.com</div>
+      <div style={styles2}>
+        &copy; 2022 Developed by:{" "}
+        <span style={{ display: size < 378 && "block", marginTop: "5px" }}>
+          Moreza.dev@gmail.com
+        </span>
+      </div>
     </div>
   );
 };
